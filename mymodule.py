04a623c67789f934a134a930d7d9b807e6b0f72e
tjddn616522-del <tjddn616522@gmail.com>
2025-09-23 17:51:58 +0900

    파이썬예제

---
+++ mymodule.py
@@ -0,0 +1,23 @@
+def my_add(a, b):
+    return a+b
+
+def my_sub(a, b):
+    return a - b
+
+class Character:
+    def __init__(self, name):
+        self.name = name
+    def speak(self):
+        print(f"저는 {self.name}입니다.")
+
+print(f"mymodule안에서의 __name__: {__name__}")
+
+
+wizard = Character("법사")
+wizard.speak()
+
+if __name__ == "__main__":
+    print(my_add(1, 2))
+    print(my_sub(3, 4))
+    wizard = Character("법사")
+    wizard.speak()
